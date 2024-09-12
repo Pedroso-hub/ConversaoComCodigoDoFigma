0cc33533af40bdc0214b974370446ba1537b9d79
Pedro Munhoz <pbmunhoz@gmail.com>
2024-09-12 16:29:51 -0300

    fiz as bandeiras e o botao swap funcionarem

--- script.js
+++ script.js
@@ -4,14 +4,28 @@ let select = document.querySelector("#select-pais");
 let select2 = document.querySelector("#select-ps");
 let img = document.querySelector("#imagem1");
 let img2 = document.querySelector("#imagem2");
-
-async function setFlag(select, img){ 
-    img.src = "https://flagsapi.com/"+select.value +"/flat/64.png";
-}
+let botaoTrocar = document.querySelector(".botton-swap");
 
 
-setFlag(select, img);
-setFlag(select2, img2);
+
+
+select.addEventListener("change", ()=> {
+    img.src = `https://flagsapi.com/${select.value}/flat/64.png`;
+});
+select2.addEventListener("change", ()=> {
+    img2.src = `https://flagsapi.com/${select2.value}/flat/64.png`;
+});
+
+
+
+botaoTrocar.addEventListener("click", ()=>{
+    let selectTemp = select.value;
+    select.value = select2.value;
+    select2.value = selectTemp;
+    let imgTemp = img.src;
+    img.src = img2.src;
+    img2.src = imgTemp;
+})
 
 
 
